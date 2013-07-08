b3630f19c8ccafd67094c5e189529d9e2c0d2035
root <root@saasbook.(none)>
2013-07-08 02:17:57 -0700

    Added first demo of 2D using GLUT

---
+++ GLUTDemo/GLUTDemo.cpp
@@ -0,0 +1,38 @@
+/*
+ * GLUTDemo.cpp
+ *
+ *  Created on: Jul 8, 2013
+ *      Author: saasbook
+ */
+#include <GL/glut.h>  // GLUT, includes glu.h and gl.h
+
+/* Handler for window-repaint event. Call back when the window first appears and
+   whenever the window needs to be re-painted. */
+void display() {
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+   glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
+
+   // Draw a Red 1x1 Square centered at origin
+   glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+      glColor3f(1.0f, 0.0f, 0.0f); // Red
+      glVertex2f(-0.5f, -0.5f);    // x, y
+      glVertex2f( 0.5f, -0.5f);
+      glVertex2f( 0.5f,  0.5f);
+      glVertex2f(-0.5f,  0.5f);
+   glEnd();
+
+   glFlush();  // Render now
+}
+
+/* Main function: GLUT runs as a console application starting at main()  */
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);                 // Initialize GLUT
+   glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
+   glutInitWindowSize(320, 320);   // Set the window's initial width & height
+   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+   glutDisplayFunc(display); // Register display callback handler for window re-paint
+   glutMainLoop();           // Enter the infinitely event-processing loop
+   return 0;
+}
+
+
